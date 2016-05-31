58305ee0de1a0233178c166ce5b337767a151e2c
hutdast <mayhem@nikensons-iMac.local>
2016-05-30 22:19:38 -0400

    customizable 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title')
+404 error
+@stop
+
+
+@section('content')
+<section class="row">
+    <h1>The page you have requested is not available</h1>
+</section>
+@stop
