c5a50aa19f8670b131e6a5c7c7452560962d9714
hutdast <hutdast@yahoo.com>
2017-05-16 23:09:17 -0400

    adding ajax to test response

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -36,6 +36,25 @@ $(function(){
 setTimeout(function(){
 $('.flash-message').remove();
 },2500);
+
+  $.ajax({
+        url: 'http://klpnfamily.com/api/test',
+        dataType: 'text',
+        type: 'post',
+        contentType: 'application/x-www-form-urlencoded',
+        
+        success: function(response, textStatus, jQxhr ){
+            alert(response );
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            alert( errorThrown );
+        }
+    });
+
+
+
+
+
 });
 
 
